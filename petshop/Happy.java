43605552d4c0b2829485bca696137e113d7f9ee1
vidhisati19 <vsati@ualberta.ca>
2026-01-07 18:43:03 -0700

    Add files via upload

---
+++ petshop/Happy.java
@@ -0,0 +1,15 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood {
+    public Happy(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String method() {
+        return "";
+    }
+
+}
---
+++ petshop/Mood.java
@@ -0,0 +1,21 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public abstract class Mood {
+    private Date date;
+
+    public Mood(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public abstract String method();
+}
